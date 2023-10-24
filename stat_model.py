264200ebf99aebef3094955ddafbe6c58cef8f4a
Andrii Iermolaiev <ai93@cornell.edu>
2023-10-24 13:32:09 -0400

    progress work

--- stat_model.py
+++ stat_model.py
@@ -3,6 +3,16 @@
 import processor as pr
 import random
 
+def process_simple(img_signals):
+    constellation = [1 + 1j, 1 - 1j, -1 + 1j, -1 - 1j]
+    bin_data = []
+    for symbol in img_signals:
+        closest_symbol = min(constellation, key=lambda x: abs(x - symbol))
+
+        symbol_index = constellation.index(closest_symbol)
+        bin_data.extend([int(bit) for bit in format(symbol_index, '02b')])
+    return bin_data
+
 def process_QPSK_image_stream_LRU(img_signals, expected):
     constellation = [1 + 1j, 1 - 1j, -1 + 1j, -1 - 1j]
     bin_data = []
@@ -80,15 +90,15 @@ def comp_bin_data(d1, d2):
  
 # Main function
 if __name__ == "__main__":
-    channel_SNR = 7
+    channel_SNR = 3
 
-    ip_1 = "data/n1.png"
+    ip_1 = "data/n2.png"
     bd_1 = pr.image_to_bits(ip_1)
     qs_1 = pr.qpsk_modulation(bd_1)
     ns_11 = pr.add_noise(qs_1, channel_SNR)
     ns_12 = pr.add_noise(qs_1, channel_SNR)
 
-    ip_2 = "data/n2.png"
+    ip_2 = "data/n3.png"
     bd_2 = pr.image_to_bits(ip_2)
     qs_2 = pr.qpsk_modulation(bd_2)
     ns_21 = pr.add_noise(qs_2, channel_SNR)
@@ -120,11 +130,15 @@ if __name__ == "__main__":
 
     # Check the differences?
     # LRU
-    print("Accuracy post demodulation with simple statistical model - LRU")
+    print("Accuracy post demodulation with simple statistical model - LRU, compared to demodulation without inference")
     print(comp_bin_data(bin_data1, bd_1))
+    print(comp_bin_data(process_simple(ns_11), bd_1))
     print(comp_bin_data(bin_data2, bd_1))
+    print(comp_bin_data(process_simple(ns_12), bd_1))
     print(comp_bin_data(bin_data3, bd_2))
+    print(comp_bin_data(process_simple(ns_21), bd_2))
     print(comp_bin_data(bin_data4, bd_2))
+    print(comp_bin_data(process_simple(ns_22), bd_2))
     
     print("Difference in progressing expectation lists")
     print(comp_bin_data(exp1, exp2))
@@ -132,11 +146,15 @@ if __name__ == "__main__":
     print(comp_bin_data(exp4, exp3))
 
     # Probabilistic substitution
-    print("Accuraccy post demodulation with simple statistical model - Probabilistic Substitution")
+    print("Accuraccy post demodulation with simple statistical model - Probabilistic Substitution, compared to demodulation without inference")
     print(comp_bin_data(bin_data12, bd_1))
+    print(comp_bin_data(process_simple(ns_11), bd_1))
     print(comp_bin_data(bin_data22, bd_1))
+    print(comp_bin_data(process_simple(ns_12), bd_1))
     print(comp_bin_data(bin_data32, bd_2))
+    print(comp_bin_data(process_simple(ns_21), bd_2))
     print(comp_bin_data(bin_data42, bd_2))
+    print(comp_bin_data(process_simple(ns_22), bd_2))
     
     print("Difference in progressing expectation lists")
     print(comp_bin_data(exp12, exp22))
